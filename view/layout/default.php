3dfd8ad602ac936f052e75515fac3b9de77aaa03
zogs <guichardsim@hotmail.com>
2014-05-14 19:21:51 +0200

    change facebook social button to share

--- view/layout/default.php
+++ view/layout/default.php
@@ -272,10 +272,10 @@ if(isset($this->OpenGraphObject)) $openGraph = $this->OpenGraphObject;;
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
 		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=153720748148187";
+		  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=153720748148187&version=v2.0";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
-
+		
 	</div>
 </body>
 
