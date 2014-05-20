d6e80f8d9ce266e5572fde2b3ff1b05ab1797ddd
Mr.zogs <guichardsim@gmail@gmail.com>
2014-05-20 11:26:28 +0200

    change bizness to professionnel

--- view/events/index.php
+++ view/events/index.php
@@ -67,7 +67,7 @@
 										<?php endif;?>
 										<?php if(isset($event->UserParticipation)): ?><span class="label ws-label ws-label-important ws-label-participe tooltipbottom" data-toggle="tooltip" title="Je participe"><i class="icon icon-white icon-thumbs-up"></i></span><?php endif; ?>
 										<?php if($event->author->isAsso()): ?><span class="label ws-label ws-label-grey tooltipbottom" data-toggle="tooltip" title="Association"><i>A</i></span><?php endif;?>
-										<?php if($event->author->isPro()): ?><span class="label ws-label ws-label-grey tooltipbottom" data-toogle="tooltip" title="Bizness"><i>P</i></span><?php endif;?>																							
+										<?php if($event->author->isPro()): ?><span class="label ws-label ws-label-grey tooltipbottom" data-toogle="tooltip" title="Professionnel"><i>P</i></span><?php endif;?>																							
 									</div>
 									<div class="events-city"><?php echo $event->getCityName();?></div>
 								</div>
